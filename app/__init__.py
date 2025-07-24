332a0bb9e63444ea11493e271f55d3e081223377
Pavel Najman <najman.pavel@gmail.com>
2025-07-24 16:47:01 +0200

    Add Flask-Migrate integration and initial migration setup
    - Initialize Flask-Migrate in the application factory.
    - Update requirements to include Flask-Migrate.
    - Create migration environment files and initial migration script for users table.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from config import config
 
 db = SQLAlchemy()
 jwt = JWTManager()
+migrate = Migrate()
 
 
 def create_app(config_name: str = "default"):
@@ -14,14 +16,11 @@ def create_app(config_name: str = "default"):
 
     db.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)  # Initialize Flask-Migrate
     api = Api(app)
 
     from .users.api import blp as UsersBlueprint
 
     api.register_blueprint(UsersBlueprint)
-
-    if config_name != "testing":
-        with app.app_context():
-            db.create_all()
 
     return app
